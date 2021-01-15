952b0c1dddab2fc882450560fb01b28f0b4d9371
yapanss <yapanss@yahoo.fr>
2021-01-15 16:05:59 +0000

    Résolu unclosing div

--- src/app/cahiertexte/cahiertexte.component.ts
+++ src/app/cahiertexte/cahiertexte.component.ts
@@ -1,111 +1,119 @@
-import { Component, OnInit, Input, Inject, ViewChild, ElementRef } from '@angular/core';
-import { CahiertexteDialogComponent } from '../cahiertexte-dialog/cahiertexte-dialog.component';
-import { MatDialog } from '@angular/material/dialog';
-import { ApiService } from '../services/api.service';
+import {
+  Component,
+  OnInit,
+  Input,
+  Inject,
+  ViewChild,
+  ElementRef,
+} from "@angular/core";
+import { CahiertexteDialogComponent } from "../cahiertexte-dialog/cahiertexte-dialog.component";
+import { MatDialog } from "@angular/material/dialog";
+import { ApiService } from "../services/api.service";
 //import { DOCUMENT } from '@angular/common';
-import { DatePipe } from '@angular/common';
+import { DatePipe } from "@angular/common";
 
 @Component({
-  selector: 'app-cahiertexte',
-  templateUrl: './cahiertexte.component.html',
-  styleUrls: ['./cahiertexte.component.css']
+  selector: "app-cahiertexte",
+  templateUrl: "./cahiertexte.component.html",
+  styleUrls: ["./cahiertexte.component.css"],
 })
 export class CahiertexteComponent implements OnInit {
   cahiertexte;
   matieres: string[] = [
-    "Anglais", "Allemand", "Arts Plastiques", "Conduite", "Couture", 
-    "Edhc", "Espagnol", "EPS", "Français", "Histoire-Géographie", 
-    "Mathématiques", "Musique", "Philosophie", "Physique-Chimie", "SVT", "TICE"
-]
+    "Anglais",
+    "Allemand",
+    "Arts Plastiques",
+    "Conduite",
+    "Couture",
+    "Edhc",
+    "Espagnol",
+    "EPS",
+    "Français",
+    "Histoire-Géographie",
+    "Mathématiques",
+    "Musique",
+    "Philosophie",
+    "Physique-Chimie",
+    "SVT",
+    "TICE",
+  ];
   matiere: string;
   dateDebut: Date;
   dateFin: Date;
   @Input() classe: string;
-  htmlContent: string =  `<div>	
+  htmlContent: string = `<div>  
                             <h1 class='bg-info'>Cahier de Texte de la classe</h1>
                             <h1 style="color: red;">Cahier de Texte de la classe</h1>
 
-                          </div>`
+                          </div>`;
   //@ViewChild('textcontenair') public textcont: ElementRef;
 
-  constructor(private dialog: MatDialog,
-              private datePipe: DatePipe,
-              private api: ApiService) { 
-              }
+  constructor(
+    private dialog: MatDialog,
+    private datePipe: DatePipe,
+    private api: ApiService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  ngOnChanges():void {
-    this.api.getOneItem('cahiertexte', this.classe)
-    .subscribe(response =>{
-      console.log('response = ', response)
-      this.cahiertexte = response['cahiertexte'];
-  
-    })
+  ngOnChanges(): void {
+    this.api.getOneItem("cahiertexte", this.classe).subscribe((response) => {
+      console.log("response = ", response);
+      this.cahiertexte = response["cahiertexte"];
+    });
   }
-  ngAfterViewInit(){
-   
-  }
+  ngAfterViewInit() {}
 
-  onCreateDialog(){
+  onCreateDialog() {
     let dialogRef = this.dialog.open(CahiertexteDialogComponent, {
-      width: '80%',
+      width: "80%",
       data: {
         classe: this.classe,
         matiere: this.matiere,
-        id: this.cahiertexte._id
-      }
-      
+        id: this.cahiertexte._id,
+      },
     });
     //dialogRef.componentInstance.addRessource.subscribe(ressource =>{
-      //this.ressources.push(ressource)
+    //this.ressources.push(ressource)
     //})
-    dialogRef.componentInstance.updateCahier.subscribe(newCahier =>{
+    dialogRef.componentInstance.updateCahier.subscribe((newCahier) => {
       this.cahiertexte = newCahier;
-    })
-  }
-  onUpdateDialog(index){
-    let dialogRef = this.dialog.open(CahiertexteDialogComponent, {
-      width: '80%',
-      
     });
-    dialogRef.componentInstance.updateCahier.subscribe(newCahier =>{
+  }
+  onUpdateDialog(index) {
+    let dialogRef = this.dialog.open(CahiertexteDialogComponent, {
+      width: "80%",
+    });
+    dialogRef.componentInstance.updateCahier.subscribe((newCahier) => {
       this.cahiertexte = newCahier;
-    })
+    });
   }
-  onDelete(index){
-   
-  }
-  onGenerate(){
+  onDelete(index) {}
+  onGenerate() {
     let contenu: string = "<div id='accordion'>";
     let dateString;
     let href;
-    let id
+    let id;
     let i = 0;
-    this.cahiertexte['partage'][this.matiere].forEach(item =>{
+    this.cahiertexte["partage"][this.matiere].forEach((item) => {
       i = i + 1;
-      href = '#item'+i;
-      id = 'item'+i;
-      dateString = this.datePipe.transform(item.date,'dd/MM/yyyy');
-      contenu = contenu + `<div class='card'>	
+      href = "#item" + i;
+      id = "item" + i;
+      dateString = this.datePipe.transform(item.date, "dd/MM/yyyy");
+      contenu =
+        contenu +
+        `<div class='card mb-2'>  
                               <div class='card-header'>
                                 <a class='btn card-link' data-toggle='collapse' href=${href}>${dateString}</a>
                               </div>
                               <div id=${id} class="collapse show" data-parent="#accordion">
                                 <div class="card-body">${item.texte}</div>
                               </div>
-                          </div>`
-    })
-    this.htmlContent = contenu + "</div>"
+                          </div>`;
+    });
+    this.htmlContent = contenu + "</div>";
   }
 
-  onChange(e){
-    
-  }
-  onDataChange(){
-    
-  }
-  
-
+  onChange(e) {}
+  onDataChange() {}
 }
